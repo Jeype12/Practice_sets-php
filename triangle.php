e7d733989717ecaf006dfecd51045fad90d793dd
Jeype12 <202280128@psu.palawan.edu.ph>
2025-02-18 10:57:13 +0800

    Added txt file and created form for the sides

---
+++ triangle.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangle Area Calculator</title>
+</head>
+
+<body>
+    <h2>Calculate the Area of a Triangle</h2>
+    <form method="post" action="">
+        <label for="side1">Side 1:</label>
+        <input type="number" step="any" name="side1" required><br><br>
+
+        <label for="side2">Side 2:</label>
+        <input type="number" step="any" name="side2" required><br><br>
+
+        <label for="side3">Side 3:</label>
+        <input type="number" step="any" name="side3" required><br><br>
+
+        <input type="submit" value="Calculate Area">
+    </form>
+</body>
+
+</html>
